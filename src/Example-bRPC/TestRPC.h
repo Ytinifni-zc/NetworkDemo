84dbdf9a5abf60bb9d5638ec0fbcbd553c4ad3d9
Ytinifni-zc <zhcheng127@gmail.com>
2021-09-14 17:37:21 +0800

    Multi_thread_echo

--- src/Example-bRPC/TestRPC.h
+++ src/Example-bRPC/TestRPC.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <bthread/bthread.h>
 #include <brpc/server.h>
 #include <brpc/channel.h>
 #include <utils/Log.h>
@@ -24,7 +25,7 @@ namespace examplebrpc {
 
     class EchoServiceImpl : public EchoService {
     public:
-        explicit EchoServiceImpl(bool attachment_) : attachment(attachment_) {}
+        EchoServiceImpl(bool attachment_, bool is_r) : attachment(attachment_), is_response(is_r) {}
 
         virtual ~EchoServiceImpl() {};
 
@@ -43,14 +44,17 @@ namespace examplebrpc {
             // how clients interact with servers more intuitively. You should
             // remove these logs in performance-sensitive servers.
 
-            LOG::info("Received request[log_id={}] from {} to {}: {} (attached={})",
-                      cntl->log_id(),
-                      butil::endpoint2str(cntl->remote_side()).c_str(),
-                      butil::endpoint2str(cntl->local_side()).c_str(),
-                      request->message(), iobuf2str(cntl->request_attachment()));
+//            LOG::info("Received request[log_id={}] from {} to {}: {} (attached={})",
+//                      cntl->log_id(),
+//                      butil::endpoint2str(cntl->remote_side()).c_str(),
+//                      butil::endpoint2str(cntl->local_side()).c_str(),
+//                      request->message(), iobuf2str(cntl->request_attachment()));
 
             // Fill response.
-            response->set_message(request->message());
+            if (is_response)
+                response->set_message(request->message());
+            else
+                std::string tmp = request->message();
 
             // You can compress the response by setting Controller, but be aware
             // that compression may be costly, evaluate before turning on.
@@ -65,18 +69,23 @@ namespace examplebrpc {
 
     private:
         bool attachment;
+        bool is_response;
 
     };
 
-    void startBRPCServer(int port, bool attachment) {
+    void startBRPCServer(int port, bool attachment, bool is_response, int max_concurrency, int internal_port) {
         brpc::Server server;
-        examplebrpc::EchoServiceImpl echo_service_impl(attachment);
+        examplebrpc::EchoServiceImpl echo_service_impl(attachment, is_response);
         if (server.AddService(&echo_service_impl, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
             LOG::error("Fail to add service");
             exit(1);
         }
         brpc::ServerOptions options;
+        options.mutable_ssl_options()->default_cert.certificate = "cert.pem";
+        options.mutable_ssl_options()->default_cert.private_key = "key.pem";
         options.idle_timeout_sec = -1;
+        options.max_concurrency = max_concurrency;
+        options.internal_port = internal_port;
         if (server.Start(port, &options) != 0) {
             LOG::error("Fail to add service");
             exit(1);
@@ -84,14 +93,65 @@ namespace examplebrpc {
         server.RunUntilAskedToQuit();
     }
 
+    bvar::LatencyRecorder g_latency_recorder("client");
+    bvar::Adder<int> g_error_count("client_error_count");
+
+    std::string g_request;
+    std::string g_attachment;
+
+    void *sender(void *arg) {
+        // Normally, you should not call a Channel directly, but instead construct
+        // a stub Service wrapping it. stub can be shared by all threads as well.
+        EchoService_Stub stub(static_cast<google::protobuf::RpcChannel *>(arg));
+
+        int log_id = 0;
+        while (!brpc::IsAskedToQuit()) {
+            // We will receive response synchronously, safe to put variables
+            // on stack.
+            EchoRequest request;
+            EchoResponse response;
+            brpc::Controller cntl;
+
+            request.set_message(g_request);
+            cntl.set_log_id(log_id++);  // set by user
+            // Set attachment which is wired to network directly instead of
+            // being serialized into protobuf messages.
+            cntl.request_attachment().append(g_attachment);
+
+            // Because `done'(last parameter) is NULL, this function waits until
+            // the response comes back or error occurs(including timedout).
+            stub.Echo(&cntl, &request, &response, NULL);
+            if (!cntl.Failed()) {
+                g_latency_recorder << cntl.latency_us();
+            } else {
+                g_error_count << 1;
+//                    CHECK(brpc::IsAskedToQuit() || !dont_fail)
+                CHECK(brpc::IsAskedToQuit())
+                << "error=" << cntl.ErrorText() << " latency=" << cntl.latency_us();
+                // We can't connect to the server, sleep a while. Notice that this
+                // is a specific sleeping to prevent this thread from spinning too
+                // fast. You should continue the business logic in a production
+                // server rather than sleeping.
+                bthread_usleep(50000);
+            }
+        }
+        return NULL;
+    }
+
     void bRPCClient(const String &server = "0.0.0.0:8000", const String &attachment = "",
                     const String &protocol = "baidu_std", const String &connection_type = "",
                     const String &load_balancer = "", int timeout_ms = 100,
-                    int max_retry = 3, int interval_ms = 1000) {
+                    int max_retry = 3, int interval_ms = 1000, bool enable_ssl = false, long attachment_size = 0,
+                    long request_size = 0, int dummy_port = 0, bool use_bthread = true, int thread_num = 1) {
+
         brpc::Channel channel;
         brpc::ChannelOptions options;
+        if (enable_ssl) {
+            options.mutable_ssl_options();
+        }
         options.protocol = protocol;
         options.connection_type = connection_type;
+        options.connect_timeout_ms = std::min(timeout_ms / 2, 100);
         options.timeout_ms = timeout_ms;
         options.max_retry = max_retry;
         LOG::debug("server: {}", server);
@@ -99,31 +159,53 @@ namespace examplebrpc {
             LOG::error("Fail to initialize channel");
             exit(1);
         }
+        if (attachment_size > 0) {
+            g_attachment.resize(attachment_size, 'a');
+        }
+        if (request_size <= 0) {
+            LOG::error("Bad request_size={}", request_size);
+            return;
+        }
+        g_request.resize(request_size, 'r');
 
-        examplebrpc::EchoService_Stub stub(&channel);
+        if (dummy_port >= 0) {
+            brpc::StartDummyServerAt(dummy_port);
+        }
 
-        int log_id = 0;
-        while (!brpc::IsAskedToQuit()) {
-            examplebrpc::EchoRequest request;
-            examplebrpc::EchoResponse response;
-            brpc::Controller cntl;
-
-            request.set_message("Hello World!");
-
-            cntl.set_log_id(log_id++);
-
-            cntl.request_attachment().append(attachment);
-            stub.Echo(&cntl, &request, &response, NULL);
-            if (!cntl.Failed()) {
-                LOG::info("Received response from {} to {}: {} (attached={}) latency={}us",
-                          butil::endpoint2str(cntl.remote_side()).c_str(),
-                          butil::endpoint2str(cntl.local_side()).c_str(), response.message(),
-                          iobuf2str(cntl.response_attachment()), cntl.latency_us());
-            } else {
-                LOG::warn("[bRPC ErrorCode: {}]{}", cntl.ErrorCode(), cntl.ErrorText());
+        std::vector<bthread_t> bids;
+        std::vector<pthread_t> pids;
+        if (!use_bthread) {
+            pids.resize(thread_num);
+            for (int i = 0; i < thread_num; ++i) {
+                if (pthread_create(&pids[i], NULL, sender, &channel) != 0) {
+                    LOG(ERROR) << "Fail to create pthread";
+                    return;
+                }
             }
-            usleep(interval_ms * 1000L);
+        } else {
+            bids.resize(thread_num);
+            for (int i = 0; i < thread_num; ++i) {
+                if (bthread_start_background(
+                        &bids[i], NULL, sender, &channel) != 0) {
+                    LOG(ERROR) << "Fail to create bthread";
+                    return;
+                }
+            }
+        }
 
+        while (!brpc::IsAskedToQuit()) {
+            sleep(1);
+            LOG(INFO) << "Sending EchoRequest at qps=" << g_latency_recorder.qps(1)
+                      << " latency=" << g_latency_recorder.latency(1);
+        }
+
+        LOG(INFO) << "EchoClient is going to quit";
+        for (int i = 0; i < thread_num; ++i) {
+            if (!use_bthread) {
+                pthread_join(pids[i], NULL);
+            } else {
+                bthread_join(bids[i], NULL);
+            }
         }
 
         LOG::info("EchoClient is goint to quit");
--- src/Server.cpp
+++ src/Server.cpp
@@ -20,8 +20,16 @@ int main(int argc, char *argv[]) {
     cmd.addArg(port_);
     Args::Arg proto_('t', "proto", true, false);
     cmd.addArg(proto_);
-    Args::Arg attachment_('a', false, false);
+    Args::Arg attachment_('a', "attachment", false, false);
     cmd.addArg(attachment_);
+    Args::Arg is_response_('b', "is_response", false, false);
+    cmd.addArg(is_response_);
+    Args::Arg idle_timeout_s_("timeout", true, false);
+    cmd.addArg(idle_timeout_s_);
+    Args::Arg max_concurrency_("max_c", true, false);
+    cmd.addArg(max_concurrency_);
+    Args::Arg internal_port_("internal_port", true, false);
+    cmd.addArg(internal_port_);
 
 
     cmd.parse();
@@ -36,10 +44,12 @@ int main(int argc, char *argv[]) {
         LOG::set_level(LOG::level::trace);
 
     auto port = port_.isDefined()? std::stoi(port_.value()) : 38888;
-
     auto proto = proto_.isDefined()? getProto(proto_.value()) : Proto::Thrift;
-
     auto attachment = attachment_.isDefined();
+    auto is_response = is_response_.isDefined();
+    auto idle_timeout_s = idle_timeout_s_.isDefined()? std::stoi(idle_timeout_s_.value()) : -1;
+    auto max_concurrency = max_concurrency_.isDefined()? std::stoi(max_concurrency_.value()) : -1;
+    auto internal_port = internal_port_.isDefined()? std::stoi(internal_port_.value()) : -1;
 
     LOG::info("Proto: {} port: {}", proto, port);
 
@@ -52,7 +62,7 @@ int main(int argc, char *argv[]) {
             break;
         case Proto::bRPC:
         {
-            examplebrpc::startBRPCServer(port, attachment);
+            examplebrpc::startBRPCServer(port, attachment, is_response, max_concurrency, internal_port);
         }
             break;
     }
